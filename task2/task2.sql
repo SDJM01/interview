cab2de592a0d34447b7e7f5d61309488c66c730d
SDJM01 <98461170+SDJM01@users.noreply.github.com>
2023-08-22 10:48:30 +0300

    Задание 2

---
+++ task2/task2.sql
@@ -0,0 +1,6 @@
+DELETE FROM products
+WHERE id NOT IN (SELECT DISTINCT product_id FROM availabilities);
+DELETE FROM stocks
+WHERE id NOT IN (SELECT DISTINCT stock_id FROM availabilities);
+DELETE FROM categories
+WHERE id NOT IN (SELECT DISTINCT category_id FROM products);
