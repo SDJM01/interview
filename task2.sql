985aaaa33fcb5787913e069f6f4e323ffb14dc10
SDJM01 <98461170+SDJM01@users.noreply.github.com>
2023-08-22 10:55:02 +0300

    Delete task2.sql

--- task2.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM products
-WHERE id NOT IN (SELECT DISTINCT product_id FROM availabilities);
-DELETE FROM stocks
-WHERE id NOT IN (SELECT DISTINCT stock_id FROM availabilities);
-DELETE FROM categories
-WHERE id NOT IN (SELECT DISTINCT category_id FROM products);
